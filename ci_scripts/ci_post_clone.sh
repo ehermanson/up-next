d029bf43068c10763a7ec4638314c06df5f111f8
Eric <eric.r.hermanson@gmail.com>
2026-02-16 16:47:00 -0500

    Bump version to 1.1 and auto-increment build number in Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,3 +11,11 @@ fi
 sed "s/YOUR_API_KEY_HERE/${TMDB_API_KEY}/" "$CI_PRIMARY_REPOSITORY_PATH/Up Next/Info.plist.template" > "$CI_PRIMARY_REPOSITORY_PATH/Up Next/Info.plist"
 
 echo "Info.plist generated successfully"
+
+# Auto-increment build number using Xcode Cloud's build number
+if [ -n "$CI_BUILD_NUMBER" ]; then
+    echo "Setting build number to $CI_BUILD_NUMBER..."
+    cd "$CI_PRIMARY_REPOSITORY_PATH"
+    agvtool new-version -all "$CI_BUILD_NUMBER"
+    echo "Build number set to $CI_BUILD_NUMBER"
+fi
